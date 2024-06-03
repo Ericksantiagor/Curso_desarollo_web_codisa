46f0d06860ae307d19e99f996386017fd7c5d2f4
Erick Santiago Rodríguez Cruz <ERODRIGUEZ@crcltd.com.do>
2024-06-03 17:46:32 -0400

    declaracion de variables

---
+++ 01-TypesOfVariables/01-TypesOfVariables/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//declarando variables
+
+string nombre = "Erick Rodriguez";
+int age = 29;
+bool isEmpleyee = true;
+double salary = 90000.50;
+
+decimal discount = 900.50m;
+
+Console.WriteLine("Hola mi nombre es: "+ nombre + " y tengo " + age + " de edad y tengo un salario de : " + salary);
+
+
+// mas tipos de variable 
+DateTime date = DateTime.Now;
+DateTime date2 = new DateTime(2024, 12, 31);
+char charater = 'a';
+byte myByte = 255;
+short myShort = -32252;//este tipo almacena un valor enterto con signo negativos
+long myLong = 123456789123456799;
+float myFloat = 120.56f;// almaceba un numero de decimal  de presion simple
+
