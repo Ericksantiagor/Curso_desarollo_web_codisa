ffa37be99225ad0cdf4dc40f2303be98163fb875
Erick Santiago Rodríguez Cruz <ERODRIGUEZ@crcltd.com.do>
2024-06-04 17:37:22 -0400

    trabajo 3

--- 03-ControlStructures/03-ControlStructures/Program.cs
+++ 03-ControlStructures/03-ControlStructures/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Estructura de seleccion o desiones
-
+/*
 int edad = 1;
 int edad2 = 15;
 
@@ -42,3 +42,39 @@ switch(letra)
         Console.WriteLine("La letra no es a ni b");
         break;
 }
+
+// bucles
+
+int i =  0 ;
+
+while (i <= 5)
+{  
+    Console.WriteLine("El Valor de i es " + i);
+    i++;
+}
+
+int j = 6 ;
+
+do
+{
+    Console.WriteLine("El Valor de j es "+ j);
+    j++;
+}while(j < 5);
+
+
+
+
+for (int k = 0; k < 5; k++)
+{
+    Console.WriteLine("El Valor de j es "+ k);
+}
+*/
+
+int[] nums = {1,2,3,4,5,6,7};
+
+foreach (int num in nums)
+{
+    Console.WriteLine("el numeero es "+ num);
+
+}
+
