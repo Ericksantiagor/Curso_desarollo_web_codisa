05a4dd90f8b046121e582f32f67ea9e826844c51
Erick Santiago Rodríguez Cruz <ERODRIGUEZ@crcltd.com.do>
2024-06-04 16:06:35 -0400

    trabajo 2

---
+++ 02-Operators/02-Operators/Program.cs
@@ -0,0 +1,48 @@
+﻿//OPERADORES ARITMETICOS
+
+int edad = 5;
+int edad2 = 30;
+int edad3 = 356;
+int resultado = edad + edad2  ;
+
+//Console.WriteLine("la sumatoria de " + edad + " y " + edad2 + " = " + resultado);
+
+Console.WriteLine($"suma: {resultado}\nValor1:{edad2}");
+
+//Operadores de asignacion
+
+int x = 1000;
+x += 2; //Es lo mismo que decir la variable x sumada con 2
+x -= 2;
+x /= 2;
+x *= 2;
+x %= 2;
+
+
+//Operador de igualdad (==)
+
+bool igualdadad = edad == edad2;
+
+
+//operador de desigualdad
+bool desigualdad = edad != edad3; 
+
+//operdado mayor que (>)
+bool  mayorque = edad > edad2;
+
+
+//operador manor que (<)
+bool menorque = edad < edad3;
+
+//operdado mayor o igual que (>)
+bool mayor_igual = edad >= edad2;
+
+//operdado menor o igual que (>)
+bool menor_igual = edad <= edad2;
+
+//Operador logicos 
+bool esEstudiante = true;
+bool tieneDescuento = false;
+int edad5 = 50;
+
+//AND OPERADOR ($$)
