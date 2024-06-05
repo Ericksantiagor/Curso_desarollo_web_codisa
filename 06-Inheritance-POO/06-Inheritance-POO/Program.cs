e7e4f8f4a3af6841e2bd522b4bb3aa7de8729ba6
Erick Santiago Rodríguez Cruz <ERODRIGUEZ@crcltd.com.do>
2024-06-05 16:09:45 -0400

    trabajo 4

---
+++ 06-Inheritance-POO/06-Inheritance-POO/Program.cs
@@ -0,0 +1,44 @@
+﻿Dog dog = new Dog("Firulais", 3, "chiguagua");
+dog.eat();
+dog.sleep();
+dog.ShowBreead();
+
+public class Animal
+{
+    protected string Name { get; set; }
+    protected int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void eat()
+    {
+        Console.WriteLine($"{Name} esta comiendo.");
+    }
+
+    public void sleep()
+    {
+        Console.WriteLine($"{Name} esta dumiendo.");
+    }
+}
+
+
+public class Dog : Animal
+{
+    public string Raza { get; set; }
+
+    public Dog(string name, int age, string raza) : base(name, age)
+    {
+        Raza = raza;
+
+    }
+
+    public void ShowBreead()
+    {
+        Console.WriteLine($"{Name} es de raza {Raza}");
+    }
+
+}
