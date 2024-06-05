314b9ed8127743ac631d67ca6ebc0c63782845f9
Erick Santiago Rodríguez Cruz <ERODRIGUEZ@crcltd.com.do>
2024-06-05 15:33:50 -0400

    Tema Encapsulacion

---
+++ 05-Encapsulation-POO/05-Encapsulation-POO/Program.cs
@@ -0,0 +1,32 @@
+﻿person person = new person("Erick",29);
+person.HelloPerson();
+public class person
+{
+    private string Name { get; set; }
+
+    private int Edad { get; set; }
+
+    public person(string name, int edad)
+    {
+        Name = name;
+        Edad = edad;
+    }
+    
+    public void HelloPerson()
+    {
+        Console.WriteLine($"Nombre:{Name}, Edad {Edad}");
+        IsAdult(Edad);
+
+    }
+    private void IsAdult( int edad)
+    {
+        if (edad > 18)
+        
+            Console.WriteLine("Es adulto");
+        else    
+            Console.WriteLine("No es adulto");
+         
+    
+    }
+
+}
